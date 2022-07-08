145c752a076e15c8cf72a3a996859da63b23d57d
akutuh <akutuh@uef.fi>
2022-07-08 15:55:52 +0300

    blogilistan frontend 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 8,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 2,
+    marginBottom: 5
+  }
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <div style={blogStyle}>
+          {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
+        </div>  
+      </div>
+      <div style={showWhenVisible}>
+        <div style={blogStyle}>
+          {blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button>
+          <br></br>{blog.url}
+          <br></br>{blog.likes} <button>like</button>
+          <br></br>{blog.user.username}
+        </div>  
+      </div>
+    </div>
+  )
+}
 
 export default Blog
